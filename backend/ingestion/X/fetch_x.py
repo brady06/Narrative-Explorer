61f1c39c3ee17bd0b0377018e9c012f9d3093e14
brady06 <bradygraff@gmail.com>
2025-08-09 11:02:00 -0400

    Fetch_X is now functional

--- backend/ingestion/X/fetch_x.py
+++ backend/ingestion/X/fetch_x.py
@@ -1,7 +1,14 @@
 from typing import List, Dict
 import snscrape.modules.twitter as sntwitter
 from datetime import datetime, timedelta
+from apify_client import ApifyClient
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+client = ApifyClient(os.getenv("APIFY_API_KEY"))
+
+# Company should be stock ticker here
 def get_x_posts(company: str, time_filter: str = "week", limit: int = 20) -> List[Dict]:
 
     # Create query
@@ -14,25 +21,48 @@ def get_x_posts(company: str, time_filter: str = "week", limit: int = 20) -> Lis
     if max_age == 0:
         print("X POST AGE ERROR")
 
-    delta = timedelta(days = max_age)
-    start_date = datetime.now() - delta
-    end_date = datetime.now()
-    query = f"{company} since:{start_date} until:{end_date} lang:en"
+    # delta = timedelta(days = max_age)
+    # start_date = datetime.now() - delta
+    # end_date = datetime.now()
+
+    end = datetime.now()
+    start = end - timedelta(days=max_age)
+    start_date = start.strftime("%Y-%m-%d_%H:%M:%S_UTC")
+    end_date = end.strftime("%Y-%m-%d_%H:%M:%S_UTC")
+
+    run_input = {
+        "maxItems": limit,
+        "searchTerms": [
+            f"${company} sentiment",
+        ],
+        "since": start_date,
+        "until": end_date,
+        "lang": "en",
+        "queryType": "Top"
+    }
+
+    # Run the actor
+    run = client.actor("CJdippxWmn9uRfooo").call(run_input=run_input)
 
     # Scraping time
     posts = []
-    post_num = 0
-    for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-        if post_num >= limit:
-            break
-
+    for tweet in client.dataset(run["defaultDatasetId"]).iterate_items():
         posts.append({
             "source": "X",
             "company_name": company,
             "title": "Not Applicable",
-            "body": tweet.content,
-            "comments": "Not Applicable"
+            "body": tweet["text"],
+            "comments": "Not Applicable",
+            "url": tweet.get("url") or tweet.get("twitterUrl") or f"https://twitter.com/i/web/status/{tweet['id']}"
         })
-        post_num += 1
 
     return posts
+
+# if __name__ == "__main__":
+#     company = "TSLA"
+#     posts = get_x_posts(company, time_filter="week", limit=20)  # pull 5 just in case
+
+#     for i, post in enumerate(posts[:20]):  # print first 2
+#         print(f"\nPost {i+1}:")
+#         print(f"Text: {post['body']}")
+#         print(f"URL: {post['url']}")
